7331c0d846a24b3e3e777b4cc8245ff1373dfe41
Darwing Medina <darwing@kronoscode.com>
2017-05-17 18:54:51 -0600

    User groups post type, metabox for admins

--- admin/class-wp_couser-admin.php
+++ admin/class-wp_couser-admin.php
@@ -73,6 +73,7 @@ class Wp_couser_Admin {
 		 * class.
 		 */
 
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'vendor/chosen/chosen.min.css', array(), $this->version, 'all' );
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp_couser-admin.css', array(), $this->version, 'all' );
 
 	}
@@ -95,9 +96,118 @@ class Wp_couser_Admin {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp_couser-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( 'chosen', plugin_dir_url( __FILE__ ) . 'vendor/chosen/chosen.jquery.min.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp_couser-admin.js', array( 'chosen', 'jquery' ), $this->version, false );
 
 	}
 
+	/**
+	 * Register Post Type: User groups.
+	 *
+	 * @since    1.0.0
+	 */
+	function register_c_user_group() {
+
+		$labels = array(
+			"name" => __( 'User groups', '' ),
+			"singular_name" => __( 'User group', '' ),
+			"all_items" => __( 'User Groups', '' ),
+		);
+
+		$args = array(
+			"label" => __( 'User groups', '' ),
+			"labels" => $labels,
+			"description" => "Collaborate User group",
+			"public" => true,
+			"publicly_queryable" => false,
+			"show_ui" => true,
+			"show_in_rest" => false,
+			"rest_base" => "",
+			"has_archive" => false,
+			"show_in_menu" => "users.php",
+			"exclude_from_search" => true,
+			"capability_type" => "post",
+			"map_meta_cap" => true,
+			"hierarchical" => false,
+			"rewrite" => array( "slug" => "c_user_group", "with_front" => true ),
+			"query_var" => true,
+			"supports" => array( "title", "editor", "thumbnail" ),
+		);
+
+		register_post_type( "c_user_group", $args );
+	}
+
+	/**
+	 * User groups Metabox
+	 * Adds a Metabox to select User groups
+	 *
+	 * @since     1.0.0
+	 */
+	public function add_user_group_admin_metabox() {
+		
+		add_meta_box( 
+			'c_user_group_admin_metabox', 
+			'Administrators', 
+			array($this,'render_user_group_admins'),
+			'c_user_group', 
+			'side', 
+			'high' 
+		);	
+	}
+	
+	/**
+	 * User groups render
+	 * Renders admin multiple select dropdown
+	 *
+	 * @since     1.0.0
+	 */
+
+	public function render_user_group_admins( $post ) {
+		
+		$role = 'administrator';
+		$key = 'c_user_group';
+ 
+		$dropdown_users_args = array(
+		    'role' => $role,
+		);
+		$users = get_users( $dropdown_users_args );
+		
+		$output = sprintf('<select multiple name="%1$s[]" id="%1$s" class="chosen">', $key);
+		 
+		$current_group_admins = get_post_meta( $post->ID, $key );
+
+		foreach ( $users as $user ) {
+			$_selected = '';
+			
+			if (in_array($user->ID, $current_group_admins)) {
+				$_selected = 'selected';
+			}
+			$output .= sprintf('<option value="%1$s" %2$s>%3$s</option>', $user->ID, $_selected, $user->user_login);
+		}
+
+		$output .= '</select>';
+
+		if ( ! empty( $output ) ) {
+			echo $output;
+		}
+	}
+	
+	/**
+	 * Save Group callback
+	 * Actions to execute when saving user group
+	 *
+	 * @since     1.0.0
+	 */
+
+	public function save_user_group_callback( $post_id, $post, $update ) {
+		error_log($_POST);
+		if( isset( $_POST['c_user_group'] ) ) {
+			delete_post_meta( $post_id, 'c_user_group' ); // Cleaning before save
+	        foreach ($_POST['c_user_group'] as $selected_user) {
+		        add_post_meta( $post_id, 'c_user_group', $selected_user );
+	        }
+	    } else {
+	        delete_post_meta( $post_id, 'c_user_group' );
+	    }
+	}
 }
--- includes/class-wp_couser.php
+++ includes/class-wp_couser.php
@@ -85,7 +85,6 @@ class Wp_couser {
 	 * - Wp_couser_Loader. Orchestrates the hooks of the plugin.
 	 * - Wp_couser_i18n. Defines internationalization functionality.
 	 * - Wp_couser_Admin. Defines all hooks for the admin area.
-	 * - Wp_couser_Public. Defines all hooks for the public side of the site.
 	 *
 	 * Create an instance of the loader which will be used to register the hooks
 	 * with WordPress.
@@ -146,6 +145,9 @@ class Wp_couser {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->add_action( 'init', $plugin_admin, 'register_c_user_group' );
+		$this->loader->add_action( 'add_meta_boxes', $plugin_admin, 'add_user_group_admin_metabox' );
+		$this->loader->add_action( 'save_post_c_user_group', $plugin_admin, 'save_user_group_callback', 10, 3);
 
 	}
 
